ac3b90594f8d8f632258784ee64aabaa0da74eae
Blair Hudson <blairhudson@users.noreply.github.com>
2026-05-24 13:52:43 +1000

    Add OpenRemote installation docs

---
+++ src/content/docs-nav.ts
@@ -0,0 +1,21 @@
+type DocsNavLink = {
+  href: string;
+  label: string;
+};
+
+type DocsNavGroup = {
+  label: string;
+  items: DocsNavLink[];
+};
+
+export const docsNavItems: (DocsNavLink | DocsNavGroup)[] = [
+  { href: "/docs/installation", label: "Installation" },
+  { href: "/docs/getting-started", label: "Getting started" },
+  {
+    label: "Self-hosting",
+    items: [
+      { href: "/docs/self-hosting/ios", label: "iOS" },
+      { href: "/docs/self-hosting/android", label: "Android" },
+    ],
+  },
+];
